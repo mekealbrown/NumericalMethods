5132d8693ead88051ee78bac8aae89fa9a86c309
Mekeal Brown <mekealbrown7@gmail.com>
2023-01-18 17:59:17 -0600

    Finished program

--- integration.cpp
+++ integration.cpp
@@ -3,6 +3,9 @@
 #include <iomanip>
 #include <iostream>
 #include <cfenv>
+#include <string>
+
+typedef double (*functions)(double);
 
 void changeRoundMethod(int x, std::string &s)
 {
@@ -26,88 +29,63 @@ void changeRoundMethod(int x, std::string &s)
     }
 }
 
-void absRelError(long double &x, long double &y, char prob, long double solution) //absolute and relative error of each solution
+//absolute and relative error of each solution
+void absRelError(double &x, double &y, char prob, double solution) 
 {
     switch (prob) {
         case '1':
-            x = (18 - solution) * -1;
+            x = std::abs(18 - solution);
             y = x / 18;
             break;
         case '2':
-            x = ((32. / 3.) - solution) * -1;
+            x = std::abs((32. / 3.) - solution);
             y = x / (32. / 3.);
             break;
         case '3':
-            x = ((M_PI / 4.) - solution) * -1;
+            x = std::abs((M_PI / 4.) - solution);
             y = x / M_PI;
             break;
         case '4':
-            x = (1. - solution) * -1;
+            x = std::abs(1. - solution); 
             y = x / 1.;
             break;
     }
 }
 
-long double firstEq(double x) {return 7 - (x * x);}
+//test functions
+double firstEq(double x) {return 7 - (x * x);}
+double secondEq(double x) {return 3 + (2 * x) - (x * x);}
+double thirdEq(double x) {return std::sqrt(1 - (x * x));}
+double fourthEq(double x) {return std::sin(x);}
 
-long double secondEq(double x) {return 3 + (2 * x) - (x * x);}
-
-long double thirdEq(double x) {return std::sqrt(1 - (x * x));}
-
-long double fourthEq(double x) {return std::sin(x);}
-
-long double riemannSum(double lower, double upper, double subInt, char which, char rsVersion)
-{
-    double dX{(upper - lower) / subInt}; //delete this and put this directly in to integration formulas
+double riemannSum(double(*fp)(double func), double lower, double upper, double subInt, char which, char rsVersion)
+{ 
     double sum{0.0};
     switch (rsVersion) {
         case 'l':   //left rule
-            for (int i{0}; i < subInt - dX; ++i)
-            {
-                if (which == '1') {sum += firstEq(lower + (i * (upper - lower) / subInt));}
-                else if (which == '2') {sum += secondEq(lower + (i * (upper - lower) / subInt));}
-                else if (which == '3') {sum += thirdEq(lower + (i * (upper - lower) / subInt));}
-                else if (which == '4') {sum += fourthEq(lower + (i * (upper - lower) / subInt));}
-            }
+            for (int i{0}; i < subInt - ((upper - lower) / subInt); ++i)
+                sum += fp(lower + (i * (upper - lower) / subInt));
             break;
         case 'r':   //right rule
             for (int i{0}; i < subInt; ++i)
-            {
-                if (which == '1') {sum += firstEq(lower + ((i + 1) * (upper - lower) / subInt));}
-                else if (which == '2') {sum += secondEq(lower + ((i + 1) * (upper - lower) / subInt));}
-                else if (which == '3') {sum += thirdEq(lower + ((i + 1) * (upper - lower) / subInt));}
-                else if (which == '4') {sum += fourthEq(lower + ((i + 1) * (upper - lower) / subInt));}
-            }
+                sum += fp(lower + ((i + 1) * (upper - lower) / subInt));
             break;
         case 'm':   //midpoint rule
             for (int i{0}; i < (subInt - ((upper - lower) / subInt / 2)); ++i)
-            {
-                if (which == '1') {sum += firstEq(lower + (i + 0.5) * (upper - lower) / subInt) * (upper - lower) / subInt;}
-                else if (which == '2') {sum += secondEq(lower + (i + 0.5) * (upper - lower) / subInt) * (upper - lower) / subInt;}
-                else if (which == '3') {sum += thirdEq(lower + (i + 0.5) * (upper - lower) / subInt) * (upper - lower) / subInt;}
-                else if (which == '4') {sum += fourthEq(lower + (i + 0.5) * (upper - lower) / subInt) * (upper - lower) / subInt ;}
-            }
+                sum += fp(lower + (i + 0.5) * (upper - lower) / subInt) * ((upper - lower) / subInt);
             return sum;
         case 't':   //trapazoidal rule
-            if (which == '1') {sum = firstEq(lower) + firstEq(upper);}
-            else if (which == '2') {sum = secondEq(lower) + secondEq(upper);}
-            else if (which == '3') {sum = thirdEq(lower) + thirdEq(upper);}
-            else if (which == '4') {sum = fourthEq(lower) + fourthEq(upper);}
+            sum = fp(lower) + fp(upper);
             for (int i{0}; i < subInt; ++i)
-            {
-                if (which == '1') {sum += 2 * firstEq(lower + i * (upper - lower) / subInt);}
-                else if (which == '2') {sum += 2 * secondEq(lower + i * (upper - lower) / subInt);}
-                else if (which == '3') {sum += 2 * thirdEq(lower + i * (upper - lower) / subInt);}
-                else if (which == '4') {sum += 2 * fourthEq(lower + i * (upper - lower) / subInt);}
-            }
+                sum += 2 * fp(lower + i * (upper - lower) / subInt);
             return sum * (0.5 * (upper - lower) / subInt);
     }
-    
-    return sum * dX;
+    return sum * ((upper - lower) / subInt);
 }
 int main()
 {
-    long double lowerBound, upperBound, subInterval;
+    double (*functions[4])(double){firstEq, secondEq, thirdEq, fourthEq}; //store each function 
+    double lowerBound, upperBound, subInterval;
     char whichFunc, rsVersion;
     int precision;
     std::cout << "Enter a lower, upper bound, and subinterval: " << std::endl;
@@ -116,19 +94,19 @@ int main()
     std::cin >> whichFunc >> rsVersion;
     std::cout << "Enter the precision: " << std::endl;   //allow variable rounding precision
     std::cin >> precision;
-    long double solution;
+    std::cout << std::endl << std::fixed;
 
-    std::cout << std::endl;
     std::string currMethod;
-    long double absolute, relative;
-    std::cout << std::fixed;
+    double solution;
+    double absolute, relative;
     for (int i{0}; i < 4; ++i) //print table showing differences in rounding methods
     {
         changeRoundMethod(i, currMethod);
-        solution = riemannSum(lowerBound, upperBound, subInterval, whichFunc , rsVersion);
+        solution = riemannSum(functions[((int)whichFunc - '0') - 1], lowerBound, upperBound, subInterval, whichFunc , rsVersion);
         absRelError(absolute, relative, whichFunc, solution);
-        std::cout << std::setprecision(precision) << '[' << currMethod << "] : " << riemannSum(lowerBound, upperBound, subInterval, whichFunc , rsVersion)
+        std::cout << std::setprecision(precision) << '[' << currMethod << "] : " << solution 
                   << std::setw(20) << std::right << "  absolute error: " << absolute << " relative error: " << relative << "%" << std::endl;
     }
     std::cout << std::endl;
+    return 0;
 }
