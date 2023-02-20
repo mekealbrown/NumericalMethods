c9b3cbc0b9b6a096fd8bd6e375b8646b6c8a0949
mekealbrown <97410284+mekealbrown@users.noreply.github.com>
2023-02-20 14:44:07 -0600

    Current State. to_string() finally working

--- floatingClass.h
+++ floatingClass.h
@@ -13,9 +13,10 @@ public:
     {
         significand_size = s;
         exponent_size = e;
-        significand = 1;
-        exponent |= 1UL << e; 
-        exponent -= 1UL;
+        significand = 1UL;
+        //exponent = 0;
+        exponent = 0 | (1UL << e) - 1UL; 
+        //exponent -= 1UL;
     };                
 
     //Ill test (52, 11, 4) first
@@ -25,39 +26,37 @@ public:
         exponent_size = e;
         sign = x > 0 ? 0 : 1;
         sign == 0 ? 0 : x *= -1;
-        exponent |= (1 << e - 1);  //exponent offset
-
-        uint32_t exp_mask{(1U << e) - 1U};
-        uint64_t sig_mask{(1UL << s) - 1UL};
-
-        uint64_t temp{static_cast<uint64_t>(x)};
-        while (temp > 0)
+        exponent = 1U << (e - 1U);
+        //set exponent
+        uint64_t temp{static_cast<uint32_t>(x)};
+        uint32_t ex_mask{(1U << e) - 1U};
+        while (temp != 1U)
         {
-            temp >>= 1UL; 
-            if (temp == 0)
-                break;
-            temp > 0UL ? ++exponent : --exponent;
-        } 
-        if (exponent >= exp_mask)
-        {
-            exponent = (exponent << e + 1U) - 1U; 
-            significand = 0;
+            temp >>= 1U;
+            ++exponent;
         }
-        
+        if (exponent >= ex_mask) //set INF 
+        {
+            exponent = ex_mask;
+            significand = 1UL;
+            return;
+        }
+        //set significand
+        significand = static_cast<uint64_t>(x);
         temp = static_cast<uint64_t>(x);
-        uint64_t max_sig{1UL << s + 1};
-        while ((temp & max_sig) == 0)
-            temp <<= 1;
-
-        significand = ~max_sig & temp; //gets trailing significand
+        uint64_t max_sig{1UL << s};
+        uint64_t sig_mask{(1UL << s) - 1UL};
+        while ((temp & max_sig) == 0Ul)
+            temp <<= 1Ul;
+        significand = (temp & sig_mask);
     };
     FP(int s,int e,std::string x);
 
-    bool isNaN()       {return exponent >= (exponent | exponent_size + 1UL) - 1UL && significand != 0;}
+    bool isNaN()       {return exponent >= (1 << exponent_size) - 1UL && significand != 0;}
     bool isZero()      {return exponent == 0 && significand == 0;}
-    bool isInfinity()  {return exponent >= (exponent << exponent_size + 1) - 1UL && significand == 0;}
+    bool isInfinity()  {return exponent >= (exponent << (exponent_size + 1UL)) - 1UL && significand == 0;}
     bool isPositive()  {return !sign;}
-    bool isNormal()    {return exponent != 0 && exponent != (exponent | exponent_size + 1UL) - 1UL && isNaN() != true;} 
+    bool isNormal()    {return exponent != 0 && exponent != (exponent | (exponent_size + 1UL)) - 1UL && isNaN() != true;} 
     bool isSubnormal() {return exponent == 0 && significand > 0 && !isNaN();}
 
     std::string to_string()
@@ -84,17 +83,22 @@ public:
         isPositive() ? result += "+" : result += "-";
         isNormal() ? result += "1." : result += "0.";
         
-        uint64_t sig_cp{significand}; uint64_t sig_cp_mask{1UL << significand_size};
-        uint32_t ex_offset{};
-        ex_offset |= (1UL << exponent_size - 1UL);
-        uint32_t ex_size{exponent - ex_offset};
- 
-        while (ex_size--)
+        uint64_t sig_mask{1UL << (significand_size - 1UL)};
+        while (sig_mask != 1)
         {
-            (sig_cp & sig_cp_mask) == 0 ? result += "0" : result += "1";
-            sig_cp_mask >>= 1UL;
+            (significand & sig_mask) == 0 ? result += "0" : result += "1";
+            sig_mask >>= 1UL;
         }
         
+
+        while (true)
+        {
+            if (*(result.end() - 1) == '1')
+                break;
+            result.erase(result.end() - 1);
+        }
+
+        uint32_t ex_offset{1U << (exponent_size - 1U)};
         result += "b" + std::to_string(exponent - ex_offset);
         return result;
     }
--- test.cpp
+++ test.cpp
@@ -4,9 +4,14 @@
 
 int main()
 {
-    FP x(52,11, 800);
-    
-
+    FP x(52,11, 129);
+   // assert(x.getExponent() == 0x7ff); // depends on the size of the exponent
+    //std::cout << "exponent " << x.getExponent() << "\n" << "sig " << x.getSignificand() << "\n";
+    assert(!x.isNaN());
+    assert(!x.isZero());
+    assert(!x.isInfinity());
+    assert(x.isNormal());
+    assert(!x.isSubnormal());
     std::cout << x.to_string() << "\n";
 }
 
