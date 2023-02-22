8a40d7753d6fb0bfee433c0220c99163e7a88a85
mekealbrown <97410284+mekealbrown@users.noreply.github.com>
2023-02-21 19:35:17 -0600

    Current State

--- test.cpp
+++ test.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 #include <cassert>
 #include "floatingClass.h"
+using namespace std;
 
 int main()
 {
-    FP x(52, 11, 1);
-   // assert(x.getExponent() == 0x7ff); // depends on the size of the exponent
-   // assert(!x.isNaN());
-    //assert(x.isZero());
-   // assert(!x.isInfinity());
-   // assert(x.isNormal());
-   // assert(!x.isSubnormal());
-    std::cout << x.to_string() << "\n";
-    assert(x.addOne());
-    std::cout << x.to_string() << "\n";
+    FP x(31,25,"+1.01110101b+9");
+    //cout << x.to_string() << endl;
+   // assert(x.addOne());
+    //cout << x.getSignificand() << endl;
+    cout << x.to_string() << endl;
 }
 
 
